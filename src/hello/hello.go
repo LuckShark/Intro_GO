b679c79cf3fb8afc4db80bcf6d045800a4ea557e
lucasaraujoalmeida <87248601+LuckShark@users.noreply.github.com>
2023-04-04 17:03:50 -0300

    aula1

---
+++ src/hello/hello.go
@@ -0,0 +1,19 @@
+/*
+pacote principal
+pacote pra imprimir
+
+função principal
+	-chama o pacote pra imprimir-imprimir hello world!
+*/
+
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("Hello Wordl!")  //isso é um Tipo (String)
+	fmt.Println("Hello caralhfffffos!")
+}
+
+//GO é ESTATICAMENTE TIPADA = sempre do mesmo jeito, com tipos sempre do mesmo jeito
+//Uma variável em GO não pode ser mudada!!!!!
